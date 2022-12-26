fd319761144372b2b22cc57b526221102f582d70
MaraScampini <109754827+MaraScampini@users.noreply.github.com>
2022-12-26 10:25:21 +0100

    [Feat] Added start script, index and router to the project

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const router = require('./router');
+const PORT = 3000;
+app.use(express.json());
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
