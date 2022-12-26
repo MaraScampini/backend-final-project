dba7fdbe7c5d3d646bbca2f50a21f939d2fbed98
MaraScampini <109754827+MaraScampini@users.noreply.github.com>
2022-12-26 10:41:46 +0100

    [Feat] Connection to database successful

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const db = require("./db/db");
+
 const app = express();
 const router = require('./router');
-const PORT = 3000;
-app.use(express.json());
-app.use(router);
+const PORT = process.env.PORT || 3000;
+const cors = require("cors");
+//Config Cors Options
+var corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+app.use(express());
+// app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  db.authenticate()
+    .then(() => {
+      console.log("Conectados a la DB");
+    })
+    .catch((error) => {
+      console.log("Se ha producido un error: " + error);
+    });
 })
