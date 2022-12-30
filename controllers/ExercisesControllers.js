d00bc4e355e35b8d579e67b5f54f206ff2549c75
Mara Scampini <109754827+MaraScampini@users.noreply.github.com>
2022-12-30 10:33:08 +0100

    [Feat] Controllers get exercises by muscle functioning

--- controllers/ExercisesControllers.js
+++ controllers/ExercisesControllers.js
@@ -3,51 +3,65 @@ require("dotenv").config();
 
 const getExercise = async (req, res) => {
   try {
-     let exercise = await models.exercises.findOne({
-       where: {
-         id_exercise: req.params.id,
-       },
-     });
-     res.json({
-       exercise,
-     });
+    let exercise = await models.exercises.findOne({
+      where: {
+        id_exercise: req.params.id,
+      },
+    });
+    res.json({
+      exercise,
+    });
   } catch (error) {
     console.error(error);
   }
- 
 };
 
 const getAllExercises = async (req, res) => {
   try {
-     let exercises = await models.exercises.findAll();
-     res.json({
-       exercises,
-     });
+    let exercises = await models.exercises.findAll();
+    res.json({
+      exercises,
+    });
   } catch (error) {
     console.error(error);
   }
- 
 };
 
 const getExerciseByMaterial = async (req, res) => {
   try {
-      const material = req.params.material;
-      let exercises = await models.exercises.findAll({
-        where: {
-          material: material,
-        },
-      });
-      res.json({
-        exercises,
-      });
+    const material = req.params.material;
+    let exercises = await models.exercises.findAll({
+      where: {
+        material: material,
+      },
+    });
+    res.json({
+      exercises,
+    });
   } catch (error) {
     console.error(error);
   }
+};
 
+const getExerciseByMuscle = async (req, res) => {
+  try {
+    const muscle = req.params.muscle;
+    let exercises = await models.exercises.findAll({
+      where: {
+        main_muscle: muscle
+      },
+    });
+    res.json({
+      exercises,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 module.exports = {
   getExercise,
   getAllExercises,
-  getExerciseByMaterial
+  getExerciseByMaterial,
+  getExerciseByMuscle
 };
--- routes/ExercisesRoutes.js
+++ routes/ExercisesRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { getExercise, getAllExercises, getExerciseByMaterial } = require("../controllers/ExercisesControllers");
+const { getExercise, getAllExercises, getExerciseByMaterial, getExerciseByMuscle } = require("../controllers/ExercisesControllers");
 const router = express.Router();
 
 router.get("/", getAllExercises);
 router.get("/:id", getExercise)
 router.get("/material/:material", getExerciseByMaterial);
+router.get("/muscle/:muscle", getExerciseByMuscle);
 
 module.exports = router;
