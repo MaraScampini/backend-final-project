54dff3d074e4a0244bdbe788c5c3f81754521459
Mara Scampini <109754827+MaraScampini@users.noreply.github.com>
2023-01-02 10:05:25 +0100

    [Feat] Delete sets controller functioning

--- controllers/SetsControllers.js
+++ controllers/SetsControllers.js
@@ -65,24 +65,64 @@ const editSet = async (req, res) => {
   }
 };
 
-const getSetsByExercise = async (req, res) => {
+const getSetsByRoutine = async (req, res) => {
   try {
-    const sets = await models.sets.findAll({
+    const routineFound = await models.routines.findOne({
       where: {
-        exerciseIdExercise: req.params.exercise,
+        id_routine: req.params.routine,
       },
     });
-    res.json({
-      message: "Sets retrieved",
-      sets,
-    });
+    if(routineFound.userIdUser === req.auth.id){
+      const sets = await models.sets.findAll({
+        where: {
+          routineIdRoutine: req.params.routine,
+        },
+      });
+
+      res.json({
+        message: "Sets retrieved",
+        sets,
+      });
+    } else {
+      res.json({
+        message: "You cannot see sets from a routine that is not yours"
+      })
+    }
   } catch (error) {
     console.error(error);
   }
 };
 
+const deleteSet = async (req, res) => {
+  try {
+    const set = req.body;
+    const routineFound = await models.routines.findOne({
+      where: {
+        id_routine: set.routine,
+      },
+    });
+    if(routineFound.userIdUser === req.auth.id){
+      await models.sets.destroy({
+        where: {
+          id_set: set.id,
+          routineIdRoutine: set.routine 
+        }
+      })
+      res.json({
+        message: "Set deleted"
+      })
+    } else {
+      res.json({
+        message: "You cannot delete sets from routines that are not yours"
+      })
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
 module.exports = {
   newSet,
   editSet,
-  getSetsByExercise
+  getSetsByRoutine,
+  deleteSet
 };
--- routes/SetsRoutes.js
+++ routes/SetsRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { newSet, editSet, getSetsByExercise } = require("../controllers/SetsControllers");
+const { newSet, editSet, getSetsByRoutine, deleteSet } = require("../controllers/SetsControllers");
 const router = express.Router();
 
 router.post("/new", newSet)
 router.patch("/edit", editSet);
-router.get("/:exercise", getSetsByExercise)
+router.get("/:routine", getSetsByRoutine)
+router.delete("/delete", deleteSet)
 
 module.exports = router;
